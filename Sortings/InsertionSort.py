3971f60467041b732244f3049b381df40574fa6b
Ashwin Raj C K <ashwinraj275@gmail.com>
2023-06-07 20:50:36 +0530

    Insertion Sort

--- Sortings/InsertionSort.py
+++ Sortings/InsertionSort.py
@@ -1,3 +1,18 @@
 def InsertionSort(arr):
 
+    for i in range(1,len(arr)):
+        j = i-1
+        key = arr[i]
+
+        while j >=0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -=1
+
+        arr[j+1] =  key
+
     return arr
+
+
+arr = [4,7,8,9,6,3,78,2,69,90,245]
+
+print(InsertionSort(arr))
